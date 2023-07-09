e01ec81e6955def3b86fd42bef318661a256d6c4
Saijyothi-10 <kjyothitrmai643@gmail.com>
2023-07-09 10:00:06 +0530

    first commit

--- src/test/java/AQE/Project2/Project2ApplicationTests.java
+++ src/test/java/AQE/Project2/Project2ApplicationTests.java
@@ -1,13 +1,30 @@
 package AQE.Project2;
 
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Project2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	public static String browser="edge";
+	public static WebDriver dr;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			if(browser.equals("firefox")) {
+				dr=new FirefoxDriver();
+			}else if(browser.equals("edge")) {
+				dr=new EdgeDriver();
+			}
+			dr.get("http://localhost:8080");
+			dr.findElement(By.id("j_username")).sendKeys("admin");
+			dr.findElement(By.name("j_password")).sendKeys("a4b25f856059493cac2d3a12fc555da2");
+			dr.findElement(By.xpath("/html/body/div/div/form/div[4]/button")).click();
+
+		}
+
 
 }
